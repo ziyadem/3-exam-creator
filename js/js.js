5321ccdfacd8c84101c9de72b7217cf9546b1469
ziyadem <maxkamovaziyadem17@gmail.com>
2022-12-02 21:08:09 +0300

    kldjf

--- js/js.js
+++ js/js.js
@@ -1,7 +1,17 @@
 let shirink = document.querySelector(".nav"),
     navTogl = document.querySelector(".togl"),
     yoqbol = document.querySelector(".yoqbol"),
-    mybutton = document.querySelector("#myBtn");
+    mybutton = document.querySelector("#myBtn"),
+    leftDes = document.querySelector(".left-design"),
+    rightDes = document.querySelector(".right-design"),
+    prjcard3 = document.querySelector(".prj-card3"),
+    prjcard2 = document.querySelector(".prj-card2"),
+    prjcard1 = document.querySelector(".prj-card1");
+    // console.log(leftDes);
+    // console.log(rightDes);
+    // console.log(prjcard3 );
+    // console.log(prjcard2 );
+    // console.log(prjcard1 );
 
 
 window.onscroll = function() {scrollFunction()};
@@ -29,3 +39,9 @@ mybutton.addEventListener("click",function(){
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }); 
+
+// let arr=[prjcard1,prjcard2,prjcard3];
+
+// rightDes.addEventListener("click" , function () {
+//    body
+// })
